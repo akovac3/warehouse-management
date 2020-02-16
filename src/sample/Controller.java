30fd034c691d4cf7725f48d77cc6d3cd3bd59f1a
Adna Kovač <akovac3@etf.unsa.ba>
2020-02-16 23:47:33 +0100

    added actions on buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,9 +3,18 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class Controller {
     public TableView<Product> tableViewProducts;
@@ -33,5 +42,40 @@ public class Controller {
         colExportPrice.setCellValueFactory(new PropertyValueFactory("exportPrice"));
         colCode.setCellValueFactory(new PropertyValueFactory("code"));
         colLocation.setCellValueFactory(new PropertyValueFactory("location"));
+    }
+
+    public void exitAction(ActionEvent actionEvent){
+        System.exit(0);
+    }
+
+    public void aboutAction(ActionEvent actionEvent){
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/about.fxml"));
+            root = loader.load();
+            stage.setTitle("About");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(true);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveAction(ActionEvent actionEvent){
+
+    }
+    public void printAction(ActionEvent actionEvent){
+
+    }
+    public void languageAction(ActionEvent actionEvent){
+
+    }
+    public void addAction(ActionEvent actionEvent){
+
+    }
+    public void moveAction(ActionEvent actionEvent){
+
     }
 }
