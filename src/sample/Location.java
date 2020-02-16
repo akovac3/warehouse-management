37203527cdd37104a47321b196c396ba4375b856
Adna Kovač <akovac3@etf.unsa.ba>
2020-02-16 14:24:07 +0100

    attributes of Location and JavaBeans spec

--- src/sample/Location.java
+++ src/sample/Location.java
@@ -1,5 +1,32 @@
 package sample;
 
 public class Location {
+    private int section;
+    private int position;
 
+    public Location(int section, int position) {
+        this.section = section;
+        this.position = position;
+    }
+
+    public Location() {
+        section=10;
+        position=1;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public void setSection(int section) {
+        this.section = section;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
