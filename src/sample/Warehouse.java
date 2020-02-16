277fa2634ea4bcd2fdf5bffe49601114a6fcf240
Adna Kovač <akovac3@etf.unsa.ba>
2020-02-16 14:10:40 +0100

    attributes of Wh and JavaBeans spec

--- src/sample/Warehouse.java
+++ src/sample/Warehouse.java
@@ -1,4 +1,67 @@
 package sample;
 
+import java.util.ArrayList;
+
 public class Warehouse {
+    private String managerName;
+    private String address;
+    private ArrayList<Location> locations;
+    private ArrayList<Product> products;
+    private String mark;
+
+    public Warehouse(String managerName, String address, ArrayList<Location> locations, ArrayList<Product> products, String mark) {
+        this.managerName = managerName;
+        this.address = address;
+        this.locations = locations;
+        this.products = products;
+        this.mark = mark;
+    }
+
+    public Warehouse() {
+        managerName=null;
+        address=null;
+        locations=null;
+        products=null;
+        mark=null;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
 }
