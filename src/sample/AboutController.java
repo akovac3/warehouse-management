fbd8c37f7e37bc03161580b4aab0d83ecff58647
Adna Kovač <akovac3@etf.unsa.ba>
2020-02-17 17:13:31 +0100

    added css

--- src/sample/AboutController.java
+++ src/sample/AboutController.java
@@ -2,22 +2,35 @@ package sample;
 
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class AboutController {
     private SimpleStringProperty managerName;
     private SimpleStringProperty address;
+    private SimpleStringProperty mark;
     private Warehouse warehouse ;
+    public ImageView imgView;
     public AboutController(Warehouse warehouse) {
         managerName= new SimpleStringProperty("");
         address = new SimpleStringProperty("");
+        mark = new SimpleStringProperty("");
         this.warehouse = warehouse;
     }
 
     @FXML
     public void initialize(){
-        managerName.set(warehouse.getManagerName());
-        address.set(warehouse.getAddress());
+        managerName.set("Manager: " + warehouse.getManagerName());
+        address.set("Address: " + warehouse.getAddress());
+        mark.set("Warehouse " + warehouse.getMark());
+        File file = new File(warehouse.getImage());
+        imgView.setImage(new Image(file.toURI().toString()));
     }
 
     public String getManagerName() {
@@ -44,5 +57,21 @@ public class AboutController {
         this.address.set(address);
     }
 
+    public String getMark() {
+        return mark.get();
+    }
 
+    public SimpleStringProperty markProperty() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark.set(mark);
+    }
+
+    public void exitAction(ActionEvent actionEvent){
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
+    }
 }
